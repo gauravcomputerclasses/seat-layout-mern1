8581e269335ab48c97013501f5d33e8f5a50a8f4
Gaurav Nawani <gauravnawani29@gmail.com>
2026-02-10 10:38:21 +0530

    Created Layout

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // ROW - A-G (7ROWS)
 // SEATS - 20 SEATS
-
 // UI JS
 // DIV
 // ID / DATA-SEATNO ATTRIBUTE
 // String.fromCharCode(num)
+
+let container = document.querySelector(".seat-layout");
+let fragment = document.createDocumentFragment();
+
+// Creating Rows
+for (let i = 65; i <= 71; i++) {
+    let row = document.createElement("div");
+    row.classList.add("seat-row");
+    let rowChar = String.fromCharCode(i);
+
+    // Creating Seats
+    for (let j = 1; j <= 20; j++) {
+        let seat = document.createElement("div");
+        seat.classList.add("seat");
+        seat.dataset.seatNumber = rowChar + j;
+        seat.innerText = rowChar + j;
+        row.appendChild(seat);
+    }
+
+    fragment.appendChild(row);
+}
+
+container.appendChild(fragment);
